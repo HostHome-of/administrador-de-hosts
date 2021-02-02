a71d8247c441daed8f241c47af2d03879d8ba271
maubg-debug <maubg2007@gmail.com>
2021-02-02 19:52:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -16,22 +16,22 @@ HOST =  config["socket"]["host"]
 PORT =  config["socket"]["puerto"]
 
 # Esto es para tests :)
-Analizar("crear|Buscador|https://github.com/maubg-debug/Buscador.git").ejecutar()
+# Analizar("crear|Buscador|https://github.com/maubg-debug/Buscador.git").ejecutar()
 
-# while True:
-#     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
-#         server.bind((HOST, PORT))
-#         server.listen()
-#         conn, addr = server.accept()
-#         with conn:
-#             print(f'Nueva conexion :: {addr}')
-#             while True:
-#                 data = conn.recv(1024).decode()
-#                 if not data:
-#                     break
+while True:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+        server.bind((HOST, PORT))
+        server.listen()
+        conn, addr = server.accept()
+        with conn:
+            print(f'Nueva conexion :: {addr}')
+            while True:
+                data = conn.recv(1024).decode()
+                if not data:
+                    break
 
-#                 msg = Analizar(data).ejecutar()
+                msg = Analizar(data).ejecutar()
 
-#                 msg = {'mensage': msg}
+                msg = {'mensage': msg}
 
-#                 conn.sendall(json.dumps(msg).encode())
+                conn.sendall(json.dumps(msg).encode())
